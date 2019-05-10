919bf113abededeb6323ad43d1ce8d2d073fc393
Anderson Tavares <andersonrochatavares@gmail.com>
2019-05-10 12:30:06 -0300

    fixing error on feature counting

--- src/learningeval/FeatureExtractor.java
+++ src/learningeval/FeatureExtractor.java
@@ -17,29 +17,60 @@ public class FeatureExtractor {
 	
 	private int numFeatures;
 	
-	private static final int MAX_MAPSIZE = 256;
-	private static final int MAX_UNITS = 50;
-	private static final int MAX_RESOURCES = 50;
+	private int maxMapSize;
+	private int maxUnits;
+	private int maxResources;
+	
+	private int maxTime;
 	
 	Map<UnitType, Integer> baseIndexes;
 	
-	public FeatureExtractor(UnitTypeTable unitTypeTable) {
-		numFeatures = 16;
+	private int initialUnitIndex;
+	private int numberTypes;
+	
+	/**
+	 * Initializes a FeatureExtractor with specified maximum values for some entities for normalizing.
+	 * The minimum is always zero.
+	 * 
+	 * @param unitTypeTable the specified types for the game
+	 * @param maxTime the maximum duration of a game, in cycles
+	 * @param maxMapLength the maximum length of any possible map (helps to differentiate small from big maps)
+	 * @param maxUnits the maximum number of possible units for the specific game
+	 * @param maxResources the maximum number of possible resources a player will possess in the specific game
+	 */
+	public FeatureExtractor(UnitTypeTable unitTypeTable, int maxTime, int maxMapLength, int maxUnits, int maxResources) {
+		/**
+		 * 1 bias, 2 map dimensions, 2 resources, 1 time, 12 unit counts 
+		 */
+		numFeatures = 18;
 		
 		baseIndexes = new HashMap<UnitType, Integer>();
 		
-		int startingUnitIndex = 5; //features regarding units start at index 5 in the feature vector
+		initialUnitIndex = 6; //features regarding units start at this index in the feature vector
+		numberTypes = 6; // there are 6 unit types (resources are not regarded)
+		
+		//TODO do as the line commented below and initialize base index by traversing the list of types
+		//numberTypes = unitTypeTable.getUnitTypes().size() - 1; 
 		
 		// baseIndexes will help to locate unit features in the array
 		// there are two features per unit type: the count for player 0 and 1
 		// hence, each unit type occupies two positions in the array and the offset increases by two 
 		// at each line below
-		baseIndexes.put(unitTypeTable.getUnitType("Worker"), startingUnitIndex);	
-		baseIndexes.put(unitTypeTable.getUnitType("Light"), startingUnitIndex + 2);
-		baseIndexes.put(unitTypeTable.getUnitType("Heavy"), startingUnitIndex + 4);
-		baseIndexes.put(unitTypeTable.getUnitType("Ranged"), startingUnitIndex + 6);
-		baseIndexes.put(unitTypeTable.getUnitType("Base"), startingUnitIndex + 8);
-		baseIndexes.put(unitTypeTable.getUnitType("Barracks"), startingUnitIndex + 10);
+		baseIndexes.put(unitTypeTable.getUnitType("Worker"), initialUnitIndex);	
+		baseIndexes.put(unitTypeTable.getUnitType("Light"), initialUnitIndex + 2);
+		baseIndexes.put(unitTypeTable.getUnitType("Heavy"), initialUnitIndex + 4);
+		baseIndexes.put(unitTypeTable.getUnitType("Ranged"), initialUnitIndex + 6);
+		baseIndexes.put(unitTypeTable.getUnitType("Base"), initialUnitIndex + 8);
+		baseIndexes.put(unitTypeTable.getUnitType("Barracks"), initialUnitIndex + 10);
+	}
+	
+	/**
+	 * Initializes the FeatureExtractor with default values for the max values.
+	 * Time is 15000, mapLength is 256, units and resources are 50
+	 * @param unitTypeTable
+	 */
+	public FeatureExtractor(UnitTypeTable unitTypeTable) {
+		this(unitTypeTable, 15000, 256, 50, 50);
 		
 	}
 	
@@ -54,12 +85,15 @@ public class FeatureExtractor {
 		features[0] = 1; //bias, always 1
 		
 		// map dimensions, capped at a maximum value
-		features[1] = Math.min(s.getPhysicalGameState().getWidth(), MAX_MAPSIZE);
-		features[2] = Math.min(s.getPhysicalGameState().getHeight(), MAX_MAPSIZE);
+		features[1] = Math.min(s.getPhysicalGameState().getWidth(), maxMapSize);
+		features[2] = Math.min(s.getPhysicalGameState().getHeight(), maxMapSize);
 		
 		// resources
-		features[3] = Math.min(s.getPlayer(player).getResources(), MAX_RESOURCES);	//player's resources
-		features[4] = Math.min(s.getPlayer(1-player).getResources(), MAX_RESOURCES);	//opponent's resources
+		features[3] = Math.min(s.getPlayer(player).getResources(), maxResources);	//player's resources
+		features[4] = Math.min(s.getPlayer(1-player).getResources(), maxResources);	//opponent's resources
+		
+		// time
+		features[5] = Math.min(s.getTime(), maxTime);
 		
 		for (Unit u : s.getPhysicalGameState().getUnits()) {
 			if (u.getType().isResource) continue; //map resources are not interesting
@@ -68,21 +102,22 @@ public class FeatureExtractor {
 			int index = baseIndexes.get(u.getType()) + u.getPlayer() == player ? 0 : 1;
 			
 			// caps the maximum number of units to aid normalization
-			features[index] = Math.min(index, MAX_UNITS);
+			features[index] = Math.min(index, maxUnits);
 		}
 		
 		// BEGIN: normalize features
 		// 1 and 2 are normalized by MAX_MAPSIZE
-		features[1] /= MAX_MAPSIZE;
-		features[2] /= MAX_MAPSIZE;
+		features[1] /= maxMapSize;
+		features[2] /= maxMapSize;
 		
 		// 3 and 4 are normalized at MAX_RESOURCES
-		features[3] /= MAX_RESOURCES;
-		features[4] /= MAX_RESOURCES;
+		features[3] /= maxResources;
+		features[4] /= maxResources;
 		
-		// 5 to 16 (inclusive) are normalized at MAX_UNITS
-		for(int i = 5; i <= 16; i++) {
-			features[i] /= MAX_UNITS;
+		// from initial unit index to 2*the number of types (inclusive) are normalized at MAX_UNITS
+		// 2*number of types is used because the count is done for each player
+		for(int i = initialUnitIndex; i <= 2*numberTypes; i++) {
+			features[i] /= maxUnits;
 		}
 		// END: normalize features
 		
