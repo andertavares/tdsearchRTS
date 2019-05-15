f10da5049a160a9f6f0d563ca55cfacdd7d189ae
Anderson Tavares <andersonrochatavares@gmail.com>
2019-05-15 11:06:23 -0300

    fixed an error in the test
    was counting the worker feature at the wrong index (was not counting
    time)

--- src/test/TestFeatureExtractor.java
+++ src/test/TestFeatureExtractor.java
@@ -33,18 +33,19 @@ class TestFeatureExtractor {
 		assertEquals(8 / 64.0, features[2]);	//height
 		assertEquals(5 / 50.0, features[3]);	//resources p0
 		assertEquals(5 / 50.0, features[4]);	//resources p1
-		assertEquals(1.0 / 64, features[5]);	//worker
-		assertEquals(1.0 / 64, features[6]);	//
-		assertEquals(0, features[7]);	//light
-		assertEquals(0, features[8]);	//
-		assertEquals(0, features[9]);	//heavy
-		assertEquals(0, features[10]);	//
-		assertEquals(0, features[11]);	//ranged
-		assertEquals(0, features[12]);	//
-		assertEquals(1.0 / 64, features[13]);	//base
-		assertEquals(1.0 / 64, features[14]);	//
-		assertEquals(0, features[15]);	//barracks
-		assertEquals(0, features[16]);	//
+		assertEquals(0., features[5]);			//time
+		assertEquals(1.0 / 64, features[6]);	//worker
+		assertEquals(1.0 / 64, features[7]);	//
+		assertEquals(0, features[8]);	//light
+		assertEquals(0, features[9]);	//
+		assertEquals(0, features[10]);	//heavy
+		assertEquals(0, features[11]);	//
+		assertEquals(0, features[12]);	//ranged
+		assertEquals(0, features[13]);	//
+		assertEquals(1.0 / 64, features[14]);	//base
+		assertEquals(1.0 / 64, features[15]);	//
+		assertEquals(0, features[16]);	//barracks
+		assertEquals(0, features[17]);	//
 		
 	}
 
