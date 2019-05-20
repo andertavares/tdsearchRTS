7f8070231fd6bf677feeeb5a48a6de74e83cc5fc
Anderson Tavares <andersonrochatavares@gmail.com>
2019-05-20 14:13:23 -0300

    added example of use

--- test-only.sh
+++ test-only.sh
@@ -7,7 +7,10 @@
 # -f, --final_rep,     Number of the final repetition (useful to parallelize executions). Assumes 0 if omitted
 # -i, --initial_rep,   Number of the initial repetition (useful to parallelize executions). Assumes 0 if omitted
 
+# example: test against LightRush from 10 previously trained repetitions  
 
+# ./test-only.sh -c config/selfplay-basesWorkers8x8.properties -t ai.abstraction.LightRush -d results/selfplay-basesWorkers8x8/ -i 0 -f 9
+ 
 classpath=.:bin:lib/*
 
 echo "Launching test matches..."
