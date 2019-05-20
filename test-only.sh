9ef548b320b38c29841f67135e55880702fd8de7
Anderson Tavares <andersonrochatavares@gmail.com>
2019-05-20 12:47:53 -0300

    added comments with cmd line params

--- test-only.sh
+++ test-only.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+# Runs test matches. Parameters:
+# -c, --config-input,  Input config path
+# -t, --test_opponent, Name of the AI to test against
+# -d, --working-dir,   Directory to load weights in and save results
+# -f, --final_rep,     Number of the final repetition (useful to parallelize executions). Assumes 0 if omitted
+# -i, --initial_rep,   Number of the initial repetition (useful to parallelize executions). Assumes 0 if omitted
+
+
 classpath=.:bin:lib/*
 
-echo "Launching experiment..."
+echo "Launching test matches..."
 
 java -classpath $classpath -Djava.library.path=lib/ main.TestOnly "$@" 
 
