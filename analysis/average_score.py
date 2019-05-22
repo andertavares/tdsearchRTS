34bc5666fe047375898d530b30cb215f2f8cae1c
Anderson Tavares <andersonrochatavares@gmail.com>
2019-05-22 14:03:43 -0300

    Map name is no longer needed. Instead, we pass this information in basedir

--- analysis/average_score.py
+++ analysis/average_score.py
@@ -21,18 +21,17 @@ def score(filename):
     return victories + 0.5 * draws
 
 
-def average_score(basedir, initial_rep, final_rep, map_name, opponent):
+def average_score(basedir, initial_rep, final_rep, opponent):
     """
     Calculates the average score in a series of repeated test matches
     against an opponent in a given map
-    :param basedir: base directory where results are stored
+    :param basedir: base directory of the experiment (the one storing rep0,rep1,etc)
     :param initial_rep:
     :param final_rep:
-    :param map_name:
     :param opponent:
     :return:
     """
-    files = [os.path.join(basedir, 'selfplay-%s' % map_name, 'rep%d' % rep, 'test-vs-%s.csv' % opponent) for rep in range(initial_rep, final_rep + 1)]
+    files = [os.path.join(basedir, 'rep%d' % rep, 'test-vs-%s.csv' % opponent) for rep in range(initial_rep, final_rep + 1)]
     #print([score(f) for f in files])
     return np.mean([score(f) for f in files])
 
@@ -41,41 +40,36 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(
         description='Calculates the average score in a series of repeated test matches '
-                    'against an opponent in a given map.'
-                    'In an experiment, the score is #victories + 0.5 * #draws.'
-                    'The script looks in directories with the pattern:'
-                    'basedir/selfplay-[map]/rep[%d]/test-vs-[opponent].csv, where %d is in [initial_rep, final_rep].'
-                    'Usage example: '
-                    'python3 analysis/average_score.py -b results -o LightRush -i 0 -f 9 -m basesWorkers8x8'
-                    'For multiple opponents:'
+                    'against an opponent in a given map.\n'
+                    'In an experiment, the score is #victories + 0.5 * #draws. '
+                    'The script looks in directories with the pattern:\n '
+                    'basedir/selfplay-[map]/rep[%d]/test-vs-[opponent].csv, where %d is in [initial_rep, final_rep].\n'
+                    'Usage example:\n'
+                    'python3 analysis/average_score.py -b results -o LightRush -i 0 -f 9 -m basesWorkers8x8\n'
+                    'For multiple opponents:\n'
                     'for o in {"HeavyRush","RangedRush","WorkerRush","LightRush"}; do echo $o; python3 analysis/average_score.py -b results -o $o -i 0 -f 9 -m basesWorkers8x8; done'
     )
 
     parser.add_argument(
-        '-b', '--basedir', help='base directory where all results are stored.'
+        'basedir', help='base directory of the experiment (the one storing rep0,rep1,etc)'
     )
 
     parser.add_argument(
-        '-i', '--initial-rep', type=int,
+        '-i', '--initial-rep', type=int, required=True,
         help='Number of initial repetition.'
     )
 
     parser.add_argument(
-        '-f', '--final-rep', type=int,
+        '-f', '--final-rep', type=int, required=True,
         help='Number of the final repetition.'
     )
 
     parser.add_argument(
-        '-o', '--opponent',
+        '-o', '--opponent', required=True,
         help='Opponent name.'
-    )
-
-    parser.add_argument(
-        '-m', '--map',
-        help='Map name.'
     )
 
     args = parser.parse_args()
 
-    print(average_score(args.basedir, args.initial_rep, args.final_rep, args.map, args.opponent))
+    print(average_score(args.basedir, args.initial_rep, args.final_rep, args.opponent))
 
