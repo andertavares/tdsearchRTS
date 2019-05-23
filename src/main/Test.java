c0b31877ee7bb67a5788484805cb9ae1a773f06f
anderson <andersonrochatavares@gmail.com>
2019-05-23 03:59:45 -0300

    renaming TestOnly -> Test

--- src/main/Test.java
+++ src/main/Test.java
@@ -21,7 +21,7 @@ import rts.units.UnitTypeTable;
 import tdsearch.SarsaSearch;
 import tdsearch.TDSearch;
 
-public class TestOnly {
+public class Test {
 	public static void main(String[] args) throws Exception {
 		Logger logger = LogManager.getRootLogger();
 		
