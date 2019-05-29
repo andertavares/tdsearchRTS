cd0e5b1453945d5a1fd3763c2cf8075ef516d45e
Anderson Tavares <andersonrochatavares@gmail.com>
2019-05-29 17:10:58 -0300

    new incomplete feature extractor

---
+++ src/features/MaterialAdvantageFeatureExtractor.java
@@ -0,0 +1,19 @@
+package features;
+
+import rts.GameState;
+
+public class MaterialAdvantageFeatureExtractor implements FeatureExtractor {
+
+	@Override
+	public int getNumFeatures() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double[] extractFeatures(GameState s, int player) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
