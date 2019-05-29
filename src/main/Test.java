8e476046d7dc886ac67436d467eb80048064af3f
anderson <andersonrochatavares@gmail.com>
2019-05-29 18:15:36 -0300

    test now uses the unified parameters

--- src/main/Test.java
+++ src/main/Test.java
@@ -8,7 +8,6 @@ import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.HelpFormatter;
-import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.apache.logging.log4j.LogManager;
@@ -16,6 +15,7 @@ import org.apache.logging.log4j.Logger;
 
 import ai.core.AI;
 import config.ConfigManager;
+import config.Parameters;
 import portfolio.PortfolioManager;
 import reward.RewardModel;
 import reward.VictoryOnly;
@@ -31,93 +31,33 @@ public class Test {
 	public static void main(String[] args) throws Exception {
 		Logger logger = LogManager.getRootLogger();
 		
-		Options options = new Options();
-
-        options.addOption(new Option("c", "config-input", true, "Input config path"));
-        options.addOption(new Option("t", "test_opponent", true, "Name of the AI to test against"));
-        options.addOption(new Option("d", "working-dir", true, "Directory to load weights in and save results"));
-        options.addOption(new Option("i", "initial_rep", true, "Number of the initial repetition (useful to parallelize executions). Assumes 0 if omitted"));
-        options.addOption(new Option("f", "final_rep", true, "Number of the final repetition (useful to parallelize executions). Assumes 0 if omitted"));
-        options.addOption(new Option("s", "save_replay", false, "If omitted, does not generate replay (trace) files."));
-        options.addOption(new Option("p", "portfolio", true, "The type of portfolio to use: basic or standard (default, does not contain support scripts)"));
-        options.addOption(new Option("r", "rewards", true, "The reward model:  winloss-tiebreak or victory-only (default)"));        
-        options.addOption(new Option("m", "test_matches", true, "Number of matches to run the test."));
-
+		Options options = Parameters.trainCommandLineOptions();
         CommandLineParser parser = new DefaultParser();
-        HelpFormatter formatter = new HelpFormatter();
         CommandLine cmd = null;
 
         try {
             cmd = parser.parse(options, args);
         } catch (ParseException e) {
             System.out.println(e.getMessage());
-            formatter.printHelp("utility-name", options);
+            new HelpFormatter().printHelp("utility-name", options);
 
             System.exit(1);
         }
 
-        String configFile = cmd.getOptionValue("config-input", "config/selfplay-example.properties");
-        String workingDir = cmd.getOptionValue("working-dir");
+        String configFile = cmd.getOptionValue("config_input");
+        String workingDir = cmd.getOptionValue("working_dir");
         
         Properties config = ConfigManager.loadConfig(configFile);
+        
+        // overrides config with command line parameters
+        Parameters.mergeCommandLineIntoProperties(cmd, config);
 		
-		// if initial and final rep were specified via command line, ignore the ones in file	
-        // TODO override in config so that these additional parameters don't need to be passed on function call
-		int initialRep = cmd.hasOption("initial_rep") ? 
-				Integer.parseInt(cmd.getOptionValue("initial_rep")) : 
-				Integer.parseInt(config.getProperty("initial_rep", "0"));
+		// retrieves initial and final reps		
+		int initialRep = Integer.parseInt(config.getProperty("initial_rep", "0"));
+		int finalRep = Integer.parseInt(config.getProperty("final_rep", "0"));
 				
-		int finalRep = cmd.hasOption("final_rep") ? 
-				Integer.parseInt(cmd.getOptionValue("final_rep")) : 
-				Integer.parseInt(config.getProperty("final_rep", "0"));
-				
-		String testPartnerName = cmd.hasOption("test_opponent") ? 
-				cmd.getOptionValue("test_opponent") : 
-				config.getProperty("test_opponent", "ai.abstraction.WorkerRush");
-				
-		// overrides the number of matches if specified via command line
-		if(cmd.hasOption("test_matches")) {
-			config.setProperty("test_matches", cmd.getOptionValue("test_matches"));
-		}
-		
-		// retrieves the portfolio from config file, with the default as basic8 (4 rush + 4 offense)
-		String csvPortfolio = config.getProperty("portfolio", "WorkerRush, LightRush, RangedRush, HeavyRush, WorkerDefense, LightDefense, RangedDefense, HeavyDefense");
-		
-		// overrides portfolio if specified via command line
-		if(cmd.hasOption("portfolio")){
-			
-			if("basic4".equals(cmd.getOptionValue("portfolio"))) {
-				logger.info("Using basic4 portfolio (only rush scripts)");
-				csvPortfolio = "WorkerRush, LightRush, RangedRush, HeavyRush";
-			}
-		
-			else if ("basic6".equals(cmd.getOptionValue("portfolio"))){
-				logger.info("Using basic6 portfolio (rush+support scripts).");
-				csvPortfolio = "WorkerRush, LightRush, RangedRush, HeavyRush, BuildBase, BuildBarracks";
-			}
-			
-			else if ("basic8".equals(cmd.getOptionValue("portfolio"))){
-				logger.info("Using basic8 portfolio (rush+defense scripts).");
-				csvPortfolio = "WorkerRush, LightRush, RangedRush, HeavyRush, WorkerDefense, LightDefense, RangedDefense, HeavyDefense";
-			}
-			
-			else if ("basic10".equals(cmd.getOptionValue("portfolio"))){
-				logger.info("Using basic10 portfolio (rush+defense+support scripts).");
-				csvPortfolio = "WorkerRush, LightRush, RangedRush, HeavyRush, WorkerDefense, LightDefense, RangedDefense, HeavyDefense, BuildBase, BuildBarracks";
-			}
-		}
-		config.setProperty("portfolio", csvPortfolio);	//stores the chosen portfolio back into config
-		
-		// checks the reward model passed
-		if(cmd.hasOption("rewards") && "winloss-tiebreak".equals(cmd.getOptionValue("rewards"))) {
-			logger.info("Using 'winloss-tiebreak' rewards");
-			config.setProperty("rewards", "winloss-tiebreak");
-		}
-		else {
-			logger.info("Using 'victory-only' rewards (1 on victory, 0 otherwise)");
-			config.setProperty("rewards", "victory-only");
-		}
-				
+		String testPartnerName = config.getProperty("test_opponent");
+						
 		boolean writeReplay = cmd.hasOption("save_replay");
 				
 		for (int rep = initialRep; rep <= finalRep; rep++) {
