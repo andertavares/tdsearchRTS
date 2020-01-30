1cf9a5411b5ccc4c802226d47d2ca10202811cce
Anderson <andersonrochatavares@gmail.com>
2020-01-30 18:46:01 -0200

    returns the number of skipped frames

--- src/utils/ForwardModel.java
+++ src/utils/ForwardModel.java
@@ -8,10 +8,14 @@ public class ForwardModel {
 	 * (a point where a player can issue an action)
 	 *
 	 * @param state
+	 * @return the number of frames moved forward
 	 */
-	public static void forward(GameState state) {
+	public static int forward(GameState state) {
+		int skippedFrames = 0;
 		while(!state.gameover() && !state.canExecuteAnyAction(0) && !state.canExecuteAnyAction(1)) {
 			state.cycle();
+			skippedFrames++;
 		}
+		return skippedFrames;
 	}
 }
