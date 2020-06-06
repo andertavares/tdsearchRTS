ccc45a15aac50f78e26b77a44bcffe115e7fc49e
anderson <andersonrochatavares@gmail.com>
2020-06-05 23:52:00 -0300

    new script to prepare a crowd of weights from different repetitions

---
+++ scripts/prepare_crowd.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+import os
+import sys
+import glob
+import shutil
+import argparse
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(
+        description='Moves weights from experiment repetitions to rep0/ dir. They will be named crowdweight-pP-mM-rR.bin (P for player position, M for the checkpoint number and x for the original rep).'
+    )
+    
+    parser.add_argument(
+        'basedir', help='Base directory of results (where all reps are)',
+    )
+    
+    parser.add_argument(
+       '-c', '--checkpoint', help='Which checkpoint shall be gathered.',
+       required=True, type=int
+    )
+
+    args = parser.parse_args()
+    
+    rep0_dir = os.path.join(args.basedir, 'rep0')
+    
+    for rep_dir in glob.glob(os.path.join(args.basedir, 'rep*')):
+        rep_num = int(rep_dir.split('/')[-1][3:])  # -1 gets rep* dir and 3: gets the *
+        #print(rep_num, rep_dir)
+        for p in [0, 1]:  # copies the files for both player positions
+            shutil.copyfile(
+                os.path.join(rep_dir, f'weights_{p}-m{args.checkpoint}.bin'), 
+                os.path.join(rep0_dir, f'crowdweight_p{p}-m{args.checkpoint}-r{rep_num}.bin')
+            )
+    print(f'Done. Check {rep0_dir}.')
+
+
+
